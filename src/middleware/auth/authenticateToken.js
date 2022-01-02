b2bb313fe0672f83476f1b5a105813a6d72ee15c
bilkn <bilkan.konus@gmail.com>
2022-01-02 13:24:08 +0300

    feat: add authentication middleware

--- src/middleware/auth/authenticateToken.js
+++ src/middleware/auth/authenticateToken.js
@@ -1,10 +1,21 @@
-import * as jwt from "jsonwebtoken";
+const jwt = require("jsonwebtoken");
 
 function authenticateToken(req, res, next) {
   const authHeader = req.headers["authorization"];
-  const token = authHeader?.split("Bearer")[1];
+  console.log("authHeader", authHeader);
+  const token = authHeader?.split(" ")[1];
 
   if (!token) res.sendStatus(401);
 
-  jwt.verify(token,process.env.ACCESS)
+  jwt.verify(token, process.env.ACCESS_TOKEN_PRIVATE_KEY, (err,user) => {
+    console.log("token: ", token);
+    if (err) {
+      console.log(err);
+      return res.status(401).send({ message: "Failed to get authorization!" });
+    }
+    req.user = user;
+    next();
+  });
 }
+
+module.exports = authenticateToken;
---
+++ src/middleware/auth/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+  authenticateToken: require("./authenticateToken"),
+};
